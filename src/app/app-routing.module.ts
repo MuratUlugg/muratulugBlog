7537cf316c90df971df5d085dbd6fca1984d3921
muratulugg <murat.ulug@kerzz.com.tr>
2020-09-11 12:04:49 +0300

    routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {
+    //www.muratulug.com/
+    path:"",
+    component:MainLayoutComponent,
+    children:
+    [
+        {
+            //www.muratulug.com/
+          path:"",
+          component:HomeComponent,
+        },
+        {
+          //www.muratulug.com/hakkimizda
+          path:"hakkimizda",
+          component:AboutMeComponent
+        },
+        {
+          //www.muratulug.com/iletisim
+          path:"iletisim",
+          component:ContactComponent
+        }
+      ]
+  },
+  {
+    //www.muratulug.com/admin
+    path:"admin",
+    component:AdminLayoutComponent
+  }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
