579e25777ebf812ee249ec2bdb562590e5e24de5
muratulugg <murat.ulug@kerzz.com.tr>
2020-09-11 14:01:10 +0300

    nav bar active link

--- src/app/nav/main-nav/main-nav.component.ts
+++ src/app/nav/main-nav/main-nav.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Router,NavigationEnd} from '@angular/router';
 
+enum MainPage
+{
+  home=1,
+  about_me=2,
+  contact=3
+}
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
@@ -7,7 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
-  constructor() { }
+  pageActive : MainPage;
+  constructor( private router:Router) //Router adında bir router tanımladık.Yapılacak işlem routerdaki her değişikliği takip etmek.
+  {
+  this.router.events.subscribe(x=>{
+    if (x instanceof NavigationEnd) // x  navigasyonun bittiği değişken ise
+    {
+      if(x.url.indexOf("home") > 0) // url home ise
+      {
+        this.pageActive = MainPage.home;
+      }
+      else if (x.url.indexOf("about")> 0)
+      {
+        this.pageActive = MainPage.about_me
+      }
+      else if (x.url.indexOf("contact")> 0)
+      {
+        this.pageActive=MainPage.contact
+      }
+      else
+      {
+        this.pageActive = MainPage.home;
+      }
+
+    }
+  })
+
+  }
+
 
   ngOnInit(): void {
   }
