04e095abeb89777732306c03f15801e3224c5661
muratulugg <murat.ulug@kerzz.com.tr>
2020-09-11 18:19:00 +0300

    title page update

--- src/app/components/page-title/page-title.component.ts
+++ src/app/components/page-title/page-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-title',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-
+  @Input() title:string;
   constructor() { }
 
   ngOnInit(): void {
