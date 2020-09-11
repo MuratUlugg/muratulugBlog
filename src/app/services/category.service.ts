055571bcfdf24c2b2b40f3fa69bf1883fbf83326
muratulugg <murat.ulug@kerzz.com.tr>
2020-09-11 14:52:19 +0300

    category service and serviceModule add

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'  // Http istekleri için import
+import {Category} from '../models/category'     // Model import
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +8,20 @@ import { Injectable } from '@angular/core';
 
 
 export class CategoryService {
+  private apiUrl:string="https://localhost:44395/api/categories";
 
-  constructor() { }
+  constructor(private httpClient:HttpClient)  {  }
+
+  public getCategories()
+  {
+    return this.httpClient.get<Category[]>(this.apiUrl);
+  }
+
+  public getCategoriesById(id:number)
+  {
+    let url = `${this.apiUrl}/${id}`   //backtick string ifadeler daha iyi  ``
+    return this.httpClient.get<Category>(url);
+  }
+
+
 }
