1a2a6377848fc27208fccd16e0c3f8b8a666a860
muratulugg <murat.ulug@kerzz.com.tr>
2020-09-11 15:30:07 +0300

    category menu edit , service connetion and data import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import {HttpClientModule} from "@angular/common/http";
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
@@ -26,7 +28,8 @@ import { MenuCategoryComponent } from './components/menu-category/menu-category.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
