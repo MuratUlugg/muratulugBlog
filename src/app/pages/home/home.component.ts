fd654cb94e8f57576fc8cd5784502186f1db98d7
muratulugg <murat.ulug@kerzz.com.tr>
2020-09-14 18:06:11 +0300

    ajax requets dead

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Article } from 'src/app/models/article';
 import { ArticleService } from 'src/app/services/article.service';
 import {Router,ActivatedRoute} from '@angular/router';
@@ -7,13 +7,16 @@ import {Router,ActivatedRoute} from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit,OnDestroy {
+  ngOnDestroy(): void{ //Sayfa geçişlerinde ajax isteklerini iptal etmek için
+    if(this.ajax != null) {this.ajax.unsubscribe();}  // İptal et
+  }
   page:number=1;
   articles:Article[];
   totalCount:number;
   pageSize:number=5;
   loadingItem:number=5;
+  ajax;
   constructor(private articleService:ArticleService,private router:Router,private route:ActivatedRoute) { }
   ngOnInit(): void {
     this.route.paramMap.subscribe(params =>{
@@ -34,7 +37,7 @@ export class HomeComponent implements OnInit {
       };
       this.articles=[];
       this.totalCount=0;
-      this.articleService.getArticle(this.page,this.pageSize).subscribe(data =>{
+      this.ajax=this.articleService.getArticle(this.page,this.pageSize).subscribe(data =>{
         this.articles=data.articles;
         this.totalCount=data.totalCount;
       })
